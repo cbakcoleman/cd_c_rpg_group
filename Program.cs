35b23a65a54f484de0e9a989cc894e843581febf
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-29 12:04:35 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cd_c_rpg_group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
